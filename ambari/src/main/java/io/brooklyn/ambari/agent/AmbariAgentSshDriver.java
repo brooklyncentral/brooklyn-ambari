503e498fda03565bedb43a9b7ee4b3fa9b3b5b88
Thomas Bouron <thomas.bouron@cloudsoftcorp.com>
2015-07-08 09:48:24 +0100

    Make the AmbariAgentSshDriver public so that classes can extend it.

--- ambari/src/main/java/io/brooklyn/ambari/agent/AmbariAgentSshDriver.java
+++ ambari/src/main/java/io/brooklyn/ambari/agent/AmbariAgentSshDriver.java
@@ -32,7 +32,7 @@ import brooklyn.location.basic.SshMachineLocation;
 import brooklyn.util.collections.MutableMap;
 import io.brooklyn.ambari.AmbariInstallCommands;
 
-class AmbariAgentSshDriver extends JavaSoftwareProcessSshDriver implements AmbariAgentDriver {
+public class AmbariAgentSshDriver extends JavaSoftwareProcessSshDriver implements AmbariAgentDriver {
     public static final Logger log = LoggerFactory.getLogger(AmbariAgentSshDriver.class);
     private final AmbariInstallCommands defaultAmbariInstallHelper = new AmbariInstallCommands(entity.getConfig(SoftwareProcess.SUGGESTED_VERSION));
 
